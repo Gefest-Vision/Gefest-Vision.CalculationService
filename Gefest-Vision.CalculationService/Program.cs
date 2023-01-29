7e9bfd81f7def9de288e335be2d201382f322e17
DeoEsor <RulianValeriy@yandex.ru>
2023-01-29 03:07:13 +0300

    Calculation Service init

---
+++ Gefest-Vision.CalculationService/Program.cs
@@ -0,0 +1,7 @@
+using Gefest_Vision.CalculationService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
